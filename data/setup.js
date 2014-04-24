7a7a9b6ea08ea849a2ed430258117d85968fd86e
Ryan Gray <jamesryangray@gmail.com>
2014-04-24 15:13:55 -0700

    Bound menu to category data as my first jquery mobile task. Also added handlebars for templating.

--- data/setup.js
+++ data/setup.js
@@ -4,12 +4,12 @@
     MyScope.db = {};
     MyScope.db.categories = [];
 
-    MyScope.db.categories.push({ key: 'Biliary', image: 'biliary.jpg', items: [], color: 'blue' });
-    MyScope.db.categories.push({ key: 'Colon', image: 'colon.jpg', items: [], color: 'green' });
-    MyScope.db.categories.push({ key: 'Esophagus', image: 'esophagus.png', items: [], color: 'red' });
-    MyScope.db.categories.push({ key: 'Pancreas', image: 'pancreas.jpg', items: [], color: 'purple' });
-    MyScope.db.categories.push({ key: 'Small Intestine', image: 'small_intestine.jpg', items: [], color: 'yellow' });
-    MyScope.db.categories.push({ key: 'Stomach', image: 'stomach.jpg', items: [], color: 'orange' });
+    MyScope.db.categories.push({ key: 'Biliary', image: 'img/procedure-icons/biliary.jpg', items: [], color: 'blue' });
+    MyScope.db.categories.push({ key: 'Colon', image: 'img/procedure-icons/colon.jpg', items: [], color: 'green' });
+    MyScope.db.categories.push({ key: 'Esophagus', image: 'img/procedure-icons/esophagus.png', items: [], color: 'red' });
+    MyScope.db.categories.push({ key: 'Pancreas', image: 'img/procedure-icons/pancreas.jpg', items: [], color: 'purple' });
+    MyScope.db.categories.push({ key: 'Small Intestine', image: 'img/procedure-icons/small_intestine.jpg', items: [], color: 'yellow' });
+    MyScope.db.categories.push({ key: 'Stomach', image: 'img/procedure-icons/stomach.jpg', items: [], color: 'orange' });
 
     MyScope.db.getProcedureById = function (procedureId) {
         if (procedureId == 'library') {
---
+++ menu.js
@@ -0,0 +1,9 @@
+﻿$(document).on('pagebeforecreate', '#home', function() {
+    var templateSource = $('#category-menu-item').html();
+    var template = Handlebars.compile(templateSource);
+
+    var listview = $('ul[data-role="listview"]').empty();
+    $.each(MyScope.db.categories, function(categoryIndex, category) {
+        listview.append(template({ category: category.key, image: category.image }));
+    });
+});
