9fbcfb29ddb2181f115037eb6b3657baff5e7975
Ryan Gray <jamesryangray@gmail.com>
2014-04-25 12:17:31 -0700

    Updated two procedures' contents

--- data/colon/emr-for-complex-polyps.js
+++ data/colon/emr-for-complex-polyps.js
@@ -9,17 +9,97 @@
             { url: 'http://www.sciencedirect.com/science/article/pii/S001651071200226X' }
         ],
         photos: [
-            'https://dl.dropboxusercontent.com/s/2fq82vhyd74eneu/channel%20colon%2023.JPEG',
-            'https://dl.dropboxusercontent.com/s/dwzzsx7bb0z8ing/channel%20colon%20final.JPEG',
-            'https://dl.dropboxusercontent.com/s/0fffb7cen9f657u/channel%20colon.JPEG',
-            'https://dl.dropboxusercontent.com/s/uu3fogsxo828ghq/colon%20emr%201.JPEG',
-            'https://dl.dropboxusercontent.com/s/yhacn5w8gtgeu3z/colon%20emr%202.JPEG',
-            'https://dl.dropboxusercontent.com/s/r72spk945s7givq/colon%20emr%20closure%20cook%20clips.JPEG',
-            'https://dl.dropboxusercontent.com/s/d4g5vwh7debfq7k/colon%20emr%20defect.JPEG',
-            'https://dl.dropboxusercontent.com/s/tuwf3jxp6xrt01k/polyp%20jan%2014.JPEG',
-            'https://dl.dropboxusercontent.com/s/gb4bqkfdzsk3h9c/post%20emr%20jan%202014.JPEG'
+            'https://dl.dropboxusercontent.com/s/psbyqqp44851qcq/channel%20colon%2023.JPEG',
+            'https://dl.dropboxusercontent.com/s/iitbw7odqebvpgb/channel%20colon%20final.JPEG',
+            'https://dl.dropboxusercontent.com/s/piyghnm22iwd0c1/channel%20colon.JPEG',
+            'https://dl.dropboxusercontent.com/s/pg5c952qmt8secc/colon%20emr%201.JPEG',
+            'https://dl.dropboxusercontent.com/s/6pt6sby23mj1vmg/colon%20emr%202.JPEG',
+            'https://dl.dropboxusercontent.com/s/xiktzmlefe5h7ht/colon%20emr%20closure%20cook%20clips.JPEG',
+            'https://dl.dropboxusercontent.com/s/mp92uphzhg1a4wv/colon%20emr%20defect.JPEG',
+            'https://dl.dropboxusercontent.com/s/g2kq4zu1vegsyw6/polyp%20jan%2014.JPEG',
+            'https://dl.dropboxusercontent.com/s/q9f8zbo98obysyv/post%20emr%20jan%202014.JPEG'
             ],
-        videos: [],
-        guidelines: []
+        videos: [
+            {
+                title: 'Video 1',
+                sources:
+                {
+                    mp4: 'https://dl.dropboxusercontent.com/s/qjqumwbqiwnpdla/Colon%20EMR%20Clip%20Closure%203.mp4.mp4',
+                    ogg: 'https://dl.dropboxusercontent.com/s/egufmv9hmzuys9d/Colon%20EMR%20Clip%20Closure%203.oggtheora.ogv',
+                    webm: 'https://dl.dropboxusercontent.com/s/u76cxtlfw9n67wu/Colon%20EMR%20Clip%20Closure%203.webmsd.webm'
+                },
+                fallback: ''
+            },
+            {
+                title: 'Video 2',
+                sources:
+                {
+                    mp4: 'https://dl.dropboxusercontent.com/s/62o09crzvg7oc1k/Colon%20Retro%20EMR.mp4.mp4',
+                    ogg: 'https://dl.dropboxusercontent.com/s/9ybv6gzh4bbmu6d/Colon%20Retro%20EMR.oggtheora.ogv',
+                    webm: 'https://dl.dropboxusercontent.com/s/t3v22fepsq0h1x6/Colon%20Retro%20EMR.webmsd.webm'
+                },
+                fallback: ''
+            },
+            {
+                title: 'Video 3',
+                sources:
+                {
+                    mp4: 'https://dl.dropboxusercontent.com/s/asa4ivhv1lpry93/Flat%20Cecal%20Polyp%20EMR%202.mp4.mp4',
+                    ogg: 'https://dl.dropboxusercontent.com/s/qfue2ed6igs67wx/Flat%20Cecal%20Polyp%20EMR%202.oggtheora.ogv',
+                    webm: 'https://dl.dropboxusercontent.com/s/6xqrcx9t3qy81cs/Flat%20Cecal%20Polyp%20EMR%202.webmsd.webm'
+                },
+                fallback: ''
+            }                                
+        ],
+            guidelines: [
+        'https://dl.dropboxusercontent.com/s/n9549wh79bnojz2/ASGE%20Colon%20EMR%20Talk_Page_01.png',
+        'https://dl.dropboxusercontent.com/s/elg8naciezmmngu/ASGE%20Colon%20EMR%20Talk_Page_02.png',
+        'https://dl.dropboxusercontent.com/s/skyp2hxrxh4ofmb/ASGE%20Colon%20EMR%20Talk_Page_03.png',
+        'https://dl.dropboxusercontent.com/s/s2ldj59rk80npuu/ASGE%20Colon%20EMR%20Talk_Page_04.png',
+        'https://dl.dropboxusercontent.com/s/5g7lhrhvrjm1r5z/ASGE%20Colon%20EMR%20Talk_Page_05.png',
+        'https://dl.dropboxusercontent.com/s/el77sbpflms3her/ASGE%20Colon%20EMR%20Talk_Page_06.png',
+        'https://dl.dropboxusercontent.com/s/jsfoq4hm2812lca/ASGE%20Colon%20EMR%20Talk_Page_07.png',
+        'https://dl.dropboxusercontent.com/s/bbrr8686imdi44c/ASGE%20Colon%20EMR%20Talk_Page_08.png',
+        'https://dl.dropboxusercontent.com/s/ttk8n16d022a9es/ASGE%20Colon%20EMR%20Talk_Page_09.png',
+        'https://dl.dropboxusercontent.com/s/d3iiyl68w4jc8zs/ASGE%20Colon%20EMR%20Talk_Page_10.png',
+        'https://dl.dropboxusercontent.com/s/qnyr4fv5hxvyham/ASGE%20Colon%20EMR%20Talk_Page_11.png',
+        'https://dl.dropboxusercontent.com/s/nr5xhflql9iiysj/ASGE%20Colon%20EMR%20Talk_Page_12.png',
+        'https://dl.dropboxusercontent.com/s/f7lw1rcb3s3xiik/ASGE%20Colon%20EMR%20Talk_Page_13.png',
+        'https://dl.dropboxusercontent.com/s/iusqb0361mekoju/ASGE%20Colon%20EMR%20Talk_Page_14.png',
+        'https://dl.dropboxusercontent.com/s/obgh3v7kvdviz8l/ASGE%20Colon%20EMR%20Talk_Page_15.png',
+        'https://dl.dropboxusercontent.com/s/q9voerzxokc2pif/ASGE%20Colon%20EMR%20Talk_Page_16.png',
+        'https://dl.dropboxusercontent.com/s/fb8rjzk6tpdgk0q/ASGE%20Colon%20EMR%20Talk_Page_17.png',
+        'https://dl.dropboxusercontent.com/s/6vwv129ilpb7vls/ASGE%20Colon%20EMR%20Talk_Page_18.png',
+        'https://dl.dropboxusercontent.com/s/bku6r1lv3rnqxg3/ASGE%20Colon%20EMR%20Talk_Page_19.png',
+        'https://dl.dropboxusercontent.com/s/8xmdhwx0oo0o6cd/ASGE%20Colon%20EMR%20Talk_Page_20.png',
+        'https://dl.dropboxusercontent.com/s/a21wzdlywha422o/ASGE%20Colon%20EMR%20Talk_Page_21.png',
+        'https://dl.dropboxusercontent.com/s/ivpvh0e7ip60fno/ASGE%20Colon%20EMR%20Talk_Page_22.png',
+        'https://dl.dropboxusercontent.com/s/jzlyx1usoz0sym0/ASGE%20Colon%20EMR%20Talk_Page_23.png',
+        'https://dl.dropboxusercontent.com/s/4y50tb43pfzezey/ASGE%20Colon%20EMR%20Talk_Page_24.png',
+        'https://dl.dropboxusercontent.com/s/ylppxqd1ax0avwh/ASGE%20Colon%20EMR%20Talk_Page_25.png',
+        'https://dl.dropboxusercontent.com/s/f0xf9li1fqumgwr/ASGE%20Colon%20EMR%20Talk_Page_26.png',
+        'https://dl.dropboxusercontent.com/s/r35289f66a89pxc/ASGE%20Colon%20EMR%20Talk_Page_27.png',
+        'https://dl.dropboxusercontent.com/s/z3cw2tft4prtnpe/ASGE%20Colon%20EMR%20Talk_Page_28.png',
+        'https://dl.dropboxusercontent.com/s/ahlt36qb91g5my0/ASGE%20Colon%20EMR%20Talk_Page_29.png',
+        'https://dl.dropboxusercontent.com/s/mpxkcndm8xxyyqh/ASGE%20Colon%20EMR%20Talk_Page_30.png',
+        'https://dl.dropboxusercontent.com/s/lu0m8dhxlh0xj06/ASGE%20Colon%20EMR%20Talk_Page_31.png',
+        'https://dl.dropboxusercontent.com/s/kpku4mq59k7l4m7/ASGE%20Colon%20EMR%20Talk_Page_32.png',
+        'https://dl.dropboxusercontent.com/s/b09izggsw3qk51o/ASGE%20Colon%20EMR%20Talk_Page_33.png',
+        'https://dl.dropboxusercontent.com/s/bom7kh98ty8tskc/ASGE%20Colon%20EMR%20Talk_Page_34.png',
+        'https://dl.dropboxusercontent.com/s/63bi0k0b4c7zo2x/ASGE%20Colon%20EMR%20Talk_Page_35.png',
+        'https://dl.dropboxusercontent.com/s/63zr1wx8g3k73kx/ASGE%20Colon%20EMR%20Talk_Page_36.png',
+        'https://dl.dropboxusercontent.com/s/7hekkw49srzia19/ASGE%20Colon%20EMR%20Talk_Page_37.png',
+        'https://dl.dropboxusercontent.com/s/o50lmanlaujwmvk/ASGE%20Colon%20EMR%20Talk_Page_38.png',
+        'https://dl.dropboxusercontent.com/s/zhd52kgqi5zjap6/ASGE%20Colon%20EMR%20Talk_Page_39.png',
+        'https://dl.dropboxusercontent.com/s/oiyuuumn7ofz5cy/ASGE%20Colon%20EMR%20Talk_Page_40.png',
+        'https://dl.dropboxusercontent.com/s/hmdv8vn22d84mp2/ASGE%20Colon%20EMR%20Talk_Page_41.png',
+        'https://dl.dropboxusercontent.com/s/kcc3xvx1ylvt7h6/ASGE%20Colon%20EMR%20Talk_Page_42.png',
+        'https://dl.dropboxusercontent.com/s/l93fsw6wh58djeo/ASGE%20Colon%20EMR%20Talk_Page_43.png',
+        'https://dl.dropboxusercontent.com/s/vx0hsb7bgf8q0mh/ASGE%20Colon%20EMR%20Talk_Page_44.png',
+        'https://dl.dropboxusercontent.com/s/bzi3r1dso36ioh5/ASGE%20Colon%20EMR%20Talk_Page_45.png',
+        'https://dl.dropboxusercontent.com/s/lodl45wkaf693ul/ASGE%20Colon%20EMR%20Talk_Page_46.png',
+        'https://dl.dropboxusercontent.com/s/cpy8rd45mbjtdaa/ASGE%20Colon%20EMR%20Talk_Page_47.png',
+        'https://dl.dropboxusercontent.com/s/mp9u61o1rczlj93/ASGE%20Colon%20EMR%20Talk_Page_48.png'
+        ]
     });
 })();
--- data/esophagus/rfa-for-barretts.js
+++ data/esophagus/rfa-for-barretts.js
@@ -9,107 +9,117 @@
             { url: 'http://www.sciencedirect.com/science/article/pii/S0016510713021408' }
         ],
         photos: [
-            'https://dl.dropboxusercontent.com/s/kyboq2qf561275o/CRIM-NBI.JPEG',
-            'https://dl.dropboxusercontent.com/s/0ho8ky7pyx94cx9/CRIM.JPEG',
-            'https://dl.dropboxusercontent.com/s/ebmrhz9pjlipyq4/GAVE-p-RFA-1.JPEG',
-            'https://dl.dropboxusercontent.com/s/k66vj7q52mtfw2n/GAVE-post-1.JPEG',
-            'https://dl.dropboxusercontent.com/s/v6arubt0p0ihbjj/GAVE-Pre-1.JPEG',
-            'https://dl.dropboxusercontent.com/s/n5e7ckvvplgo0dy/image-1.jpeg',
-            'https://dl.dropboxusercontent.com/s/8ozo1ad3dlmp6el/image.jpeg',
-            'https://dl.dropboxusercontent.com/s/o90hon1s1y8aoyh/jc-post-1.JPEG',
-            'https://dl.dropboxusercontent.com/s/krgpklvoy8pgly5/jc-post2.JPEG',
-            'https://dl.dropboxusercontent.com/s/r9rv16vmfnqq9n1/JC-pre-2.JPEG',
-            'https://dl.dropboxusercontent.com/s/ydfscqpbbtvq8wz/JC-pre1.JPEG'
+            'https://dl.dropboxusercontent.com/s/n5ov61a6qqo57ch/CRIM-NBI.JPEG',
+            'https://dl.dropboxusercontent.com/s/2m7us04q9mp3pjh/CRIM.JPEG',
+            'https://dl.dropboxusercontent.com/s/3wtfndv9bdpewpq/GAVE-p-RFA-1.JPEG',
+            'https://dl.dropboxusercontent.com/s/rk55ta40zcw4caw/GAVE-post-1.JPEG',
+            'https://dl.dropboxusercontent.com/s/e0xk6z9ugj0ivw9/GAVE-Pre-1.JPEG',
+            'https://dl.dropboxusercontent.com/s/16hnz7oe1cy8fve/image-1.jpeg',
+            'https://dl.dropboxusercontent.com/s/4xpw34vt2wmm47w/image.jpeg',
+            'https://dl.dropboxusercontent.com/s/67an2pgtcnpog5r/jc-post2.JPEG',
+            'https://dl.dropboxusercontent.com/s/3yuyx251s5zuvcp/jc-post-1.JPEG',
+            'https://dl.dropboxusercontent.com/s/wz4pta5o9dqnq0d/JC-pre1.JPEG',
+            'https://dl.dropboxusercontent.com/s/btmxs9f2n9zomtg/JC-pre-2.JPEG'
         ],
         videos: [
             {
-                title: 'This is some video',
+                title: 'Video 1',
                 sources:
                 {
-                    mp4: 'https://dl.dropboxusercontent.com/s/xq57ukw5l0jdlt7/AVM-RFA-2.mp4',
-                    ogg: 'https://dl.dropboxusercontent.com/s/5x28hmlc4jqrx4y/AVM-RFA-2.ogg',
-                    webm: 'https://dl.dropboxusercontent.com/s/ue3693xnhllsv1l/AVM-RFA-2.webm'
+                    mp4: 'https://dl.dropboxusercontent.com/s/8l4dt8p8na4v3vv/AVM%20RFA%202.mp4.mp4',
+                    ogg: 'https://dl.dropboxusercontent.com/s/nrqenn9rv509aj0/AVM%20RFA%202.oggtheora.ogv',
+                    webm: 'https://dl.dropboxusercontent.com/s/ng8pryy5sxly39r/AVM%20RFA%202.webmsd.webm'
                 },
-                fallback: 'https://www.dropbox.com/s/xq57ukw5l0jdlt7/AVM-RFA-2.mp4'
+                fallback: ''
             },
             {
-                title: 'This is some other video',
+                title: 'Video 2',
                 sources:
                 {
-                    mp4: 'https://dl.dropboxusercontent.com/s/xq57ukw5l0jdlt7/AVM-RFA-2.mp4',
-                    ogg: 'https://dl.dropboxusercontent.com/s/5x28hmlc4jqrx4y/AVM-RFA-2.ogg',
-                    webm: 'https://dl.dropboxusercontent.com/s/ue3693xnhllsv1l/AVM-RFA-2.webm'
+                    mp4: 'https://dl.dropboxusercontent.com/s/kdl6703law2ew3a/Barrett%27s%20Esophagus%20360%20RFA%202.mp4.mp4',
+                    ogg: 'https://dl.dropboxusercontent.com/s/w13rkjwh89459z5/Barrett%27s%20Esophagus%20360%20RFA%202.oggtheora.ogv',
+                    webm: 'https://dl.dropboxusercontent.com/s/63z945am29eirxa/Barrett%27s%20Esophagus%20360%20RFA%202.webmsd.webm'
                 },
-                fallback: 'https://www.dropbox.com/s/xq57ukw5l0jdlt7/AVM-RFA-2.mp4'
+                fallback: ''
             },
             {
-                title: 'This is yet another video',
+                title: 'Video 3',
                 sources:
                 {
-                    mp4: 'https://dl.dropboxusercontent.com/s/xq57ukw5l0jdlt7/AVM-RFA-2.mp4',
-                    ogg: 'https://dl.dropboxusercontent.com/s/5x28hmlc4jqrx4y/AVM-RFA-2.ogg',
-                    webm: 'https://dl.dropboxusercontent.com/s/ue3693xnhllsv1l/AVM-RFA-2.webm'
+                    mp4: 'https://dl.dropboxusercontent.com/s/ber9rdikf5ou6xj/Duette%20EMR%20Bleed%203.mp4.mp4',
+                    ogg: 'https://dl.dropboxusercontent.com/s/bjn3l0mpvfg7n9d/Duette%20EMR%20Bleed%203.oggtheora.ogv',
+                    webm: 'https://dl.dropboxusercontent.com/s/c1mc4qr4n1ugj6t/Duette%20EMR%20Bleed%203.webmsd.webm'
                 },
-                fallback: 'https://www.dropbox.com/s/xq57ukw5l0jdlt7/AVM-RFA-2.mp4'
+                fallback: ''
             },
             {
-                title: 'This is some video',
+                title: 'Video 4',
                 sources:
                 {
-                    mp4: 'https://dl.dropboxusercontent.com/s/xq57ukw5l0jdlt7/AVM-RFA-2.mp4',
-                    ogg: 'https://dl.dropboxusercontent.com/s/5x28hmlc4jqrx4y/AVM-RFA-2.ogg',
-                    webm: 'https://dl.dropboxusercontent.com/s/ue3693xnhllsv1l/AVM-RFA-2.webm'
+                    mp4: 'https://dl.dropboxusercontent.com/s/rwys91uejaeh9bd/Focal%20RFA%20Technique%202.mp4.mp4',
+                    ogg: 'https://dl.dropboxusercontent.com/s/nxavsibkkgfy9y3/Focal%20RFA%20Technique%202.oggtheora.ogv',
+                    webm: 'https://dl.dropboxusercontent.com/s/ai6e3op72h5078j/Focal%20RFA%20Technique%202.webmsd.webm'
                 },
-                fallback: 'https://www.dropbox.com/s/xq57ukw5l0jdlt7/AVM-RFA-2.mp4'
+                fallback: ''
             },
             {
-                title: 'This is some video',
+                title: 'Video 5',
                 sources:
                 {
-                    mp4: 'https://dl.dropboxusercontent.com/s/xq57ukw5l0jdlt7/AVM-RFA-2.mp4',
-                    ogg: 'https://dl.dropboxusercontent.com/s/5x28hmlc4jqrx4y/AVM-RFA-2.ogg',
-                    webm: 'https://dl.dropboxusercontent.com/s/ue3693xnhllsv1l/AVM-RFA-2.webm'
+                    mp4: 'https://dl.dropboxusercontent.com/s/rrgzpogmom0g43v/Gave%20%281280%20x%20720%29.mp4.mp4',
+                    ogg: 'https://dl.dropboxusercontent.com/s/3w0vq4adx5eknf4/Gave%20%281280%20x%20720%29.oggtheora.ogv',
+                    webm: 'https://dl.dropboxusercontent.com/s/kapexl5cvrv5q4p/Gave%20%281280%20x%20720%29.webmsd.webm'
                 },
-                fallback: 'https://www.dropbox.com/s/xq57ukw5l0jdlt7/AVM-RFA-2.mp4'
+                fallback: ''
+            },
+            {
+                title: 'Video 6',
+                sources:
+                {
+                    mp4: 'https://dl.dropboxusercontent.com/s/xvvu87be3biojjb/RFA%20SCC%202.mp4.mp4',
+                    ogg: 'https://dl.dropboxusercontent.com/s/f7h9wyny06kt1fd/RFA%20SCC%202.oggtheora.ogv',
+                    webm: 'https://dl.dropboxusercontent.com/s/sp6l2a0gtd1rw40/RFA%20SCC%202.webmsd.webm'
+                },
+                fallback: ''
             }
         ],
         guidelines: [
-            'https://dl.dropboxusercontent.com/s/1jadkygm2uvvafp/Slide01.jpg',
-            'https://dl.dropboxusercontent.com/s/ezfdj0d6lltglsk/Slide02.jpg',
-            'https://dl.dropboxusercontent.com/s/bzsw2gd5u5oaen9/Slide03.jpg',
-            'https://dl.dropboxusercontent.com/s/npqxgik46amdebz/Slide04.jpg',
-            'https://dl.dropboxusercontent.com/s/p51se5k11hfx013/Slide05.jpg',
-            'https://dl.dropboxusercontent.com/s/ol38fek5jesml9l/Slide06.jpg',
-            'https://dl.dropboxusercontent.com/s/uaz4n3fxouz78nq/Slide07.jpg',
-            'https://dl.dropboxusercontent.com/s/w6nj4f188qoggcf/Slide08.jpg',
-            'https://dl.dropboxusercontent.com/s/elr3cqzck6f6tom/Slide09.jpg',
-            'https://dl.dropboxusercontent.com/s/5vlqrejfsaqhfdx/Slide10.jpg',
-            'https://dl.dropboxusercontent.com/s/wtvteg0hkarnga6/Slide11.jpg',
-            'https://dl.dropboxusercontent.com/s/gmimqzl8xrivo9l/Slide12.jpg',
-            'https://dl.dropboxusercontent.com/s/1e4xiznvr2uh7bu/Slide13.jpg',
-            'https://dl.dropboxusercontent.com/s/qksl1tsr75vbnwk/Slide14.jpg',
-            'https://dl.dropboxusercontent.com/s/3jgzlt97k30fkh8/Slide15.jpg',
-            'https://dl.dropboxusercontent.com/s/ugf5lpjstxcl2t2/Slide16.jpg',
-            'https://dl.dropboxusercontent.com/s/1sh2wikhmensjmi/Slide17.jpg',
-            'https://dl.dropboxusercontent.com/s/cwl1calizlied4y/Slide18.jpg',
-            'https://dl.dropboxusercontent.com/s/um09qn76n3o9t22/Slide19.jpg',
-            'https://dl.dropboxusercontent.com/s/vx1xq2in4s06264/Slide20.jpg',
-            'https://dl.dropboxusercontent.com/s/q5mdtbup99xal7u/Slide21.jpg',
-            'https://dl.dropboxusercontent.com/s/hqtibj4jskakwlm/Slide22.jpg',
-            'https://dl.dropboxusercontent.com/s/awlijpgsfdguakh/Slide23.jpg',
-            'https://dl.dropboxusercontent.com/s/sr95ilxw9j1y7vk/Slide24.jpg',
-            'https://dl.dropboxusercontent.com/s/00ip8wue5x3txkw/Slide25.jpg',
-            'https://dl.dropboxusercontent.com/s/bt2vuk6smol8rbh/Slide26.jpg',
-            'https://dl.dropboxusercontent.com/s/vs0a7s11d4h9nc2/Slide27.jpg',
-            'https://dl.dropboxusercontent.com/s/bs158rb7uhlfti7/Slide28.jpg',
-            'https://dl.dropboxusercontent.com/s/aj46j3zxfjfk62h/Slide29.jpg',
-            'https://dl.dropboxusercontent.com/s/2anzywv8xtisqms/Slide30.jpg',
-            'https://dl.dropboxusercontent.com/s/dhnddb6y9chcaqa/Slide31.jpg',
-            'https://dl.dropboxusercontent.com/s/ay70bb8hqvg967o/Slide32.jpg',
-            'https://dl.dropboxusercontent.com/s/zzkcaqnr6esb1h6/Slide33.jpg',
-            'https://dl.dropboxusercontent.com/s/vde5sq6jsdfixnt/Slide34.jpg',
-            'https://dl.dropboxusercontent.com/s/y7uwyh7x7arx84j/Slide35.jpg',
-            'https://dl.dropboxusercontent.com/s/64hqkkoo099k9te/Slide36.jpg'
+            'https://dl.dropboxusercontent.com/s/izc27bedy0o7aw0/Slide01.jpg',
+            'https://dl.dropboxusercontent.com/s/f3zjl2fa77yeu07/Slide02.jpg',
+            'https://dl.dropboxusercontent.com/s/ew43aqxq41ane20/Slide03.jpg',
+            'https://dl.dropboxusercontent.com/s/bb4rzoy41ziiqx9/Slide04.jpg',
+            'https://dl.dropboxusercontent.com/s/bypamt5ylknjp6u/Slide05.jpg',
+            'https://dl.dropboxusercontent.com/s/6kr9gqo6l0mpqlh/Slide06.jpg',
+            'https://dl.dropboxusercontent.com/s/u4oazz46sqa42sn/Slide07.jpg',
+            'https://dl.dropboxusercontent.com/s/xsjfzk7r3s54xpe/Slide08.jpg',
+            'https://dl.dropboxusercontent.com/s/57moy582qn6q1jz/Slide09.jpg',
+            'https://dl.dropboxusercontent.com/s/85tdm8if5gzl37e/Slide10.jpg',
+            'https://dl.dropboxusercontent.com/s/wjvwbqbsvcg9vh7/Slide11.jpg',
+            'https://dl.dropboxusercontent.com/s/p6k0vzs41d6iqcb/Slide12.jpg',
+            'https://dl.dropboxusercontent.com/s/hutszpz6dbhsw5l/Slide13.jpg',
+            'https://dl.dropboxusercontent.com/s/ncvmcioe3cxbey8/Slide14.jpg',
+            'https://dl.dropboxusercontent.com/s/31ud2tu0ybci308/Slide15.jpg',
+            'https://dl.dropboxusercontent.com/s/py57xrp5yt4sul9/Slide16.jpg',
+            'https://dl.dropboxusercontent.com/s/w9bhhp4cnb7ca77/Slide17.jpg',
+            'https://dl.dropboxusercontent.com/s/abvcfu9jpaujngw/Slide18.jpg',
+            'https://dl.dropboxusercontent.com/s/hzaxxc1hkqq5xe5/Slide19.jpg',
+            'https://dl.dropboxusercontent.com/s/cxf6xiq3o1qopkp/Slide20.jpg',
+            'https://dl.dropboxusercontent.com/s/6ikowzfqb1toqdp/Slide21.jpg',
+            'https://dl.dropboxusercontent.com/s/ozfou6zw7c9kzol/Slide22.jpg',
+            'https://dl.dropboxusercontent.com/s/5chrj4qlfchonrw/Slide23.jpg',
+            'https://dl.dropboxusercontent.com/s/69uq5d26axe9qf1/Slide24.jpg',
+            'https://dl.dropboxusercontent.com/s/81mj903qp852i2h/Slide25.jpg',
+            'https://dl.dropboxusercontent.com/s/lr13uu7q447uz14/Slide26.jpg',
+            'https://dl.dropboxusercontent.com/s/kqpxxihqnldcwiu/Slide27.jpg',
+            'https://dl.dropboxusercontent.com/s/gmq9w0zchv6kxa5/Slide28.jpg',
+            'https://dl.dropboxusercontent.com/s/3qed6uree8lsifp/Slide29.jpg',
+            'https://dl.dropboxusercontent.com/s/zjaeqegbfk7thb0/Slide30.jpg',
+            'https://dl.dropboxusercontent.com/s/n1372cetkduvuau/Slide31.jpg',
+            'https://dl.dropboxusercontent.com/s/ig1nfr63eahsslt/Slide32.jpg',
+            'https://dl.dropboxusercontent.com/s/ei9r3lrkzud0064/Slide33.jpg',
+            'https://dl.dropboxusercontent.com/s/kcw0omxodj47quh/Slide34.jpg',
+            'https://dl.dropboxusercontent.com/s/3zu822nvqi1q8v8/Slide35.jpg',
+            'https://dl.dropboxusercontent.com/s/p11m4ve5jy2la91/Slide36.jpg'
         ]
     });
 })();
