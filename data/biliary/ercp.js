8f95e25d2a23150041c2787bc89ea8da35f34846
Ryan Gray <jamesryangray@gmail.com>
2014-04-26 17:01:36 -0700

    Added posters and titles for videos

--- data/biliary/ercp.js
+++ data/biliary/ercp.js
@@ -20,6 +20,61 @@
             'https://dl.dropboxusercontent.com/s/48nxh750qyq9h94/image.png'
         ],
         videos: [
+        {
+            title: 'ERCP Diverticulum',
+            poster: 'img/video_posters/biliary/ercp/ercp-diverticulum.jpg',
+            sources:
+            {
+                mp4: 'https://dl.dropboxusercontent.com/s/ga3zyjb0dugat10/ERCP%20Challenges%20Giant%20Stones%20EHL.mp4.mp4',
+                ogg: 'https://dl.dropboxusercontent.com/s/miyhqeab5iacn85/ERCP%20Challenges%20Giant%20Stones%20EHL.oggtheora.ogv',
+                webm: 'https://dl.dropboxusercontent.com/s/3h3uqd5lnt9ojcf/ERCP%20Challenges%20Giant%20Stones%20EHL.webmsd.webm'
+            },
+            fallback: ''
+        },
+        {
+            title: 'ERCP Giant Stones EHL',
+            poster: 'img/video_posters/biliary/ercp/ercp-giant-stones-ehl.jpg',
+            sources:
+            {
+                mp4: 'https://dl.dropboxusercontent.com/s/j36s6y1n9h9w48m/ERCP%20diverticulum.mp4.mp4',
+                ogg: 'https://dl.dropboxusercontent.com/s/ehte44mcg0is4pw/ERCP%20diverticulum.oggtheora.ogv',
+                webm: 'https://dl.dropboxusercontent.com/s/l0euiw3757gkcmu/ERCP%20diverticulum.webmsd.webm'
+            },
+            fallback: ''
+        },
+        {
+            title: 'EUS Biliary Rendezvous for Malignant Obstruction',
+            poster: 'img/video_posters/biliary/ercp/eus-biliary-rendezvous-for-malignant-obstruction.jpg',
+            sources:
+            {
+                mp4: 'https://dl.dropboxusercontent.com/s/jgaik284smxne5o/EUS%20Biliary%20Rendezvous%20for%20Malignant%20Obstruction.mp4.mp4',
+                ogg: 'https://dl.dropboxusercontent.com/s/vetvrlcdeg5hk44/EUS%20Biliary%20Rendezvous%20for%20Malignant%20Obstruction.oggtheora.ogv',
+                webm: 'https://dl.dropboxusercontent.com/s/eu77ssyvfq5n9ba/EUS%20Biliary%20Rendezvous%20for%20Malignant%20Obstruction.webmsd.webm'
+            },
+            fallback: ''
+        },
+        {
+            title: 'Pancreatoscopy',
+            poster: 'img/video_posters/biliary/ercp/pancreatoscopy.jpg',
+            sources:
+            {
+                mp4: 'https://dl.dropboxusercontent.com/s/af161deslshylos/pancreatoscopy.mp4.mp4',
+                ogg: 'https://dl.dropboxusercontent.com/s/tnrp4o1r580p4s3/pancreatoscopy.oggtheora.ogv',
+                webm: 'https://dl.dropboxusercontent.com/s/np2pv6cgm7bc6e5/pancreatoscopy.webmsd.webm'
+            },
+            fallback: ''
+        },
+        {
+            title: 'Spyglass',
+            poster: 'img/video_posters/biliary/ercp/spyglass.jpg',
+            sources:
+            {
+                mp4: 'https://dl.dropboxusercontent.com/s/y9o7dp8d5ar891k/spyglass.mp4.mp4',
+                ogg: 'https://dl.dropboxusercontent.com/s/nc4ntg0si5xghhf/spyglass.oggtheora.ogv',
+                webm: 'https://dl.dropboxusercontent.com/s/tbvnblnvxm427tt/spyglass.webmsd.webm'
+            },
+            fallback: ''
+        }
         ],
         guidelines: [
         ]
