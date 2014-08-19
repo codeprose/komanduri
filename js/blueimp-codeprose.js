8ed6e75b30171f82931a49b21864c8b9597dd747
Ryan Gray <jamesryangray@gmail.com>
2014-08-19 10:47:59 -0700

    Removed comments

--- js/blueimp-codeprose.js
+++ js/blueimp-codeprose.js
@@ -1,17 +1,8 @@
 ﻿blueimp.Gallery.prototype.textFactory = function (obj, callback) {
-    /*
-    
-+        <div class="video-container">
-+            <iframe width="560" height="315" src="//www.youtube.com/embed/S0Yu1nvmRMM?rel=0"
-+                frameborder="0" allowfullscreen></iframe>
-+        </div>
-    
-    */
-
     var $iframe = $('<iframe style="height: 100%" width="100%" frameborder="0" allowfullscreen>');
     $iframe.attr('src', 'http://www.youtube.com/embed/' + 'zuxZ0Ar35TA' + '?rel=0');
 
-    var $container = $('<div style="width: 100%; height: 100%">');
+    var $container = $('<div>');
     $container.append($iframe);
 
     this.setTimeout(callback, [{
@@ -20,14 +11,4 @@
     }]);
 
     return $container[0];
-
-//    var $test = $('<div>');
-//    $test.html('<span>THIS IS A TEST</span>');
-
-//    this.setTimeout(callback, [{
-//        type: 'load',
-//        target: $test.get(0)
-//    }]);
-
-//    return $test.get(0);
 };
