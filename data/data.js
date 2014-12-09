8f540ec9fbf8ce8dd5b20fb4371deb6e2b7b852d
Ryan Gray <jamesryangray@gmail.com>
2014-12-09 12:34:29 -0800

    updating with daniel's latest

---
+++ data/data.js
@@ -0,0 +1,11 @@
+﻿var EndoscopyNow = EndoscopyNow || {};
+
+EndoscopyNow.Categories = EndoscopyNow.Categories || {};
+EndoscopyNow.Procedures = EndoscopyNow.Procedures || {};
+
+EndoscopyNow.Procedures['biliary']          = [{ 'title': 'Difficult ERCP', 'procedure-id': 'ercp' }];
+EndoscopyNow.Procedures['colon']            = [{ 'title': 'EMR for Complex Polyps', 'procedure-id': 'polyps'}];
+EndoscopyNow.Procedures['esophagus']        = [];
+EndoscopyNow.Procedures['pancreas']         = [];
+EndoscopyNow.Procedures['small-intestine']  = [];
+EndoscopyNow.Procedures['stomach']          = [];
